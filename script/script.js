00c27156a134bcf9fd777b7a42b96571c9886da7
Igor Angelyuk <femistokl123angelyuk@yandex.ru>
2023-02-16 15:48:35 +0300

    mesto

--- script/script.js
+++ script/script.js
@@ -1,30 +1,30 @@
-const popup = document.querySelector('.popup');
-const Edit_button = document.querySelector('.profile-main__edit-button');
-const author = document.querySelector('.profile-main__author');
-const job = document.querySelector('.profile-main__job');
+const popupContent = document.querySelector('.popup');
+const editButton = document.querySelector('.profile-main__edit-button');
+const authorElement = document.querySelector('.profile-main__author');
+const jobElement = document.querySelector('.profile-main__job');
 const nameInput = document.querySelector('.popup__input_type_name');
 const infInput = document.querySelector('.popup__input_type_inf');
-const close = document.querySelector('.popup__close');
-const form = document.querySelector('.popup__form');
+const closeButton = document.querySelector('.popup__close');
+const formElement = document.querySelector('.popup__form');
 
-function OpenForm() {
-    popup.classList.toggle('popup_opened');
+function openForm() {
+    popupContent.classList.toggle('popup_opened');
     nameInput.value = author.textContent;
     infInput.value = job.textContent;
 }
 
-function CloseForm() {
-    popup.classList.toggle('popup_opened');
+function closeForm() {
+    popupContent.classList.toggle('popup_opened');
 }
 
 function handleFormSubmit(evt) {
     evt.preventDefault()
-    author.textContent = nameInput.value;
-    job.textContent = infInput.value;
-    popup.classList.toggle('popup_opened');
+    authorElement.textContent = nameInput.value;
+    jobElement.textContent = infInput.value;
+    popupContent.classList.toggle('popup_opened');
 
 }
 
-Edit_button.addEventListener("click", OpenForm);
-close.addEventListener("click", CloseForm);
-form.addEventListener("submit", handleFormSubmit);
+editButton.addEventListener("click", openForm);
+closeButton.addEventListener("click", closeForm);
+formElement.addEventListener("submit", handleFormSubmit);
