22ca268716503c9d491fcc2ca6e478589ec9d6f7
Igor Angelyuk <femistokl123angelyuk@yandex.ru>
2023-04-25 20:15:08 +0300

    mesto

--- validate/validate.js
+++ validate/validate.js
@@ -4,7 +4,7 @@ enableValidation({
   submitButtonSelector: '.popup__button',
   inactiveButtonClass: 'popup__button_disabled',
   inputErrorClass: 'popup__input_type_error',
-  errorClass: 'popup__error_visible'
+  errorClass: 'popup__input-error'
 });
 
 function showInputError(inputElement, formElement, inputErrorClass, errorClass, errorMessage) {
