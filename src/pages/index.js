524e3fd3cada16938a826935183cc3c96b98d6e0
Femistokl-123 <femistokl123angelyuk@yandex.ru>
2023-10-18 04:39:40 +0300

    mesto

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,13 @@
-import FormValidator from "./FormValidator.js";
-import Card from "./card.js";
+import FormValidator from "../components/FormValidator.js";
+import Card from "../components/Card.js";
 import { initialCards } from "../utils/constant.js";
 import { validationSettings } from "../utils/constant.js";
-import "./pages/index.css";
+import "./index.css";
 
-import Section from "./Section.js";
-import PopupWithImage from "./PopupWithImage.js";
-import PopupWithForm from "./PopupWithForm.js";
-import UserInfo from "./UserInfo.js";
+import Section from "../components/Section.js";
+import PopupWithImage from "../components/PopupWithImage.js";
+import PopupWithForm from "../components/PopupWithForm.js";
+import UserInfo from "../components/UserInfo.js";
 
 const popupProfile = document.querySelector('.popup_profile');
 const popupAddCard = document.querySelector('.popup_add');
