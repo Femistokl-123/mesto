16f4554524438bcca9054c1e14171cabac35f595
Femistokl-123 <femistokl123angelyuk@yandex.ru>
2023-10-25 11:09:16 +0300

    mesto

--- src/components/PopupConfirmDelete.js
+++ src/components/PopupConfirmDelete.js
@@ -3,7 +3,7 @@ import Popup from "./Popup.js";
 export default class PopupConfirmDelete extends Popup {
     constructor(popup) {
         super(popup);
-        this._submitButton = this._popup.querySelector('.popup__button_add');
+        this._submitButton = this._popup.querySelector('.popup__button_save');
     }
 
     setSubmitAction(action) {
